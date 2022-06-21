f6d42caf37b9acd5e791a16621e13f64b006d551
Beauchard Tewann <tbeauchard@faddef.math.u-bordeaux1.fr>
2022-06-21 16:14:47 +0200

    code ajusté pour distinguer les deux classes d'âges au sein des données de comptage et permettant la sortie des boxplots d'intérêts

--- IPM_juv_adult_counts.R
+++ IPM_juv_adult_counts.R
@@ -255,7 +255,7 @@ model {
 # ---------------------
 # Number of simulations
 # nsim <- 1500
-nsim <- 10# ~~~ for testing
+nsim <- 5# ~~~ for testing
 
 # Age specific survival probabilities (juv, adult)
 sj <- 0.3
@@ -364,7 +364,7 @@ load("Data Fig 6.4.Rdata")
 incl <- which(res1[1,8,]<1.05 & res1[2,8,]<1.05 & res1[4,8,]<1.05 & res1[35,8,]<1.05 & res1[45,8,]<1.05 & res1[54,8,]<1.05 &
                 res2[1,8,]<1.05 & res2[2,8,]<1.05 & res2[4,8,]<1.05 & res2[35,8,]<1.05 & res2[45,8,]<1.05 & res2[54,8,]<1.05 &
                 res3[1,8,]<1.05 & res3[2,8,]<1.05 & res3[4,8,]<1.05 & res3[35,8,]<1.05 & res3[45,8,]<1.05 & res3[54,8,]<1.05 &
-                res4[1,8,]<1.05 & res4[2,8,]<1.05 & res4[4,8,]<1.05 & res4[35,8,]<1.05 &re41[45,8,]<1.05 & res4[54,8,]<1.05)
+                res4[1,8,]<1.05 & res4[2,8,]<1.05 & res4[4,8,]<1.05 & res4[35,8,]<1.05 &res4[45,8,]<1.05 & res4[54,8,]<1.05)
 if(length(incl) > 1000)
   incl <- incl[1:1000]
 
@@ -372,7 +372,7 @@ op <- par(las=1, mar=c(2.5, 4.2, 1, 1), mfrow=c(6, 2))
 name <- c("CR & P & C", "CR & C", "P & C", "C")
 
 lab <- expression('Juvenile survival ('*italic('s')[italic(j)]*')')
-boxplot(cbind(res1[1,1,], res2[1,1,], res3[1,1,], res4[1,1,]),
+boxplot(cbind(res1[1,1,incl], res2[1,1,incl], res3[1,1,incl], res4[1,1,incl]),
         ylab=lab, outline=FALSE, names=NA,
         col=c("red", "dodgerblue", "darkolivegreen", "orange"), axes=FALSE)
 abline(h=sj, lty=2)
@@ -380,14 +380,14 @@ axis(1, labels=NA)
 axis(2)
 
 lab <- expression('SD ('*italic('s')[italic(j)]*')')
-boxplot(cbind(res1[1,2,], res2[1,2,], res3[1,2,], res4[1,2,]),
+boxplot(cbind(res1[1,2,incl], res2[1,2,incl], res3[1,2,incl], res4[1,2,incl]),
         ylab=lab, outline=FALSE, names=NA,
         col=c("red", "dodgerblue", "darkolivegreen", "orange"), axes=FALSE)
 axis(1, labels=NA)
 axis(2)
 
 lab <- expression('Adult survival ('*italic('s')[italic(a)]*')')
-boxplot(cbind(res1[2,1,], res2[2,1,], res3[2,1,], res4[2,1,]),
+boxplot(cbind(res1[2,1,incl], res2[2,1,incl], res3[2,1,incl], res4[2,1,incl]),
         ylab=lab, outline=FALSE, names=NA,
         col=c("red", "dodgerblue", "darkolivegreen", "orange"), axes=FALSE)
 abline(h=sa, lty=2)
@@ -395,14 +395,14 @@ axis(1, labels=NA)
 axis(2)
 
 lab <- expression('SD ('*italic('s')[italic(a)]*')')
-boxplot(cbind(res1[2,2,], res2[2,2,], res3[2,2,], res4[2,2,]),
+boxplot(cbind(res1[2,2,incl], res2[2,2,incl], res3[2,2,incl], res4[2,2,incl]),
         ylab=lab, outline=FALSE, names=NA,
         col=c("red", "dodgerblue", "darkolivegreen", "orange"), axes=FALSE)
 axis(1, labels=NA)
 axis(2)
 
 lab <- expression('Productivity ('*italic('f')*')')
-boxplot(cbind(res1[4,1,], res2[4,1,], res3[4,1,], res4[4,1,]),
+boxplot(cbind(res1[4,1,incl], res2[4,1,incl], res3[4,1,incl], res4[4,1,incl]),
         ylab=lab, outline=FALSE, names=NA,
         col=c("red", "dodgerblue", "darkolivegreen", "orange"), axes=FALSE)
 abline(h=fl1, lty=2)
@@ -410,7 +410,7 @@ axis(1, labels=NA)
 axis(2)
 
 lab <- expression('SD ('*italic('f')*')')
-boxplot(cbind(res1[4,2,], res2[4,2,], res3[4,2,], res4[4,2,]),
+boxplot(cbind(res1[4,2,incl], res2[4,2,incl], res3[4,2,incl], res4[4,2,incl]),
         ylab=lab, outline=FALSE, names=NA,
         col=c("red", "dodgerblue", "darkolivegreen", "orange"), axes=FALSE)
 axis(1, labels=NA)
@@ -419,14 +419,14 @@ axis(2)
 
 
 lab <- expression('Juvenile counts ('*italic('C')[italic(j)]*')')
-boxplot(cbind(res1[35,1,], res2[35,1,], res3[35,1,], res4[35,1,]),
+boxplot(cbind(res1[35,1,incl], res2[35,1,incl], res3[35,1,incl], res4[35,1,incl]),
         ylab=lab,  outline= FALSE, names=name,
         col=c("red", "dodgerblue", "darkolivegreen", "orange"), axes=FALSE)
 axis(1, labels=name, at=1:4)
 axis(2)
 
 lab <- expression('SD ('*italic('C')[italic(j)]*')')
-boxplot(cbind(res1[35,2,], res2[35,2,], res3[35,2,], res4[35,2,]),
+boxplot(cbind(res1[35,2,incl], res2[35,2,incl], res3[35,2,incl], res4[35,2,incl]),
         ylab=lab,  outline=FALSE, names=name,
         col=c("red", "dodgerblue", "darkolivegreen", "orange"), axes=FALSE)
 axis(1, labels=name, at=1:4)
@@ -434,28 +434,28 @@ axis(2)
 
 
 lab <- expression('Adult counts ('*italic('C')[italic(ad)]*')')
-boxplot(cbind(res1[45,1,], res2[45,1,], res3[45,1,], res4[45,1,]),
+boxplot(cbind(res1[45,1,incl], res2[45,1,incl], res3[45,1,incl], res4[45,1,incl]),
         ylab=lab,  outline= FALSE, names=name,
         col=c("red", "dodgerblue", "darkolivegreen", "orange"), axes=FALSE)
 axis(1, labels=name, at=1:4)
 axis(2)
 
 lab <- expression('SD ('*italic('C')[italic(ad)]*')')
-boxplot(cbind(res1[45,2,], res2[45,2,], res3[45,2,], res4[45,2,]),
+boxplot(cbind(res1[45,2,incl], res2[45,2,incl], res3[45,2,incl], res4[45,2,incl]),
         ylab=lab,  outline=FALSE, names=name,
         col=c("red", "dodgerblue", "darkolivegreen", "orange"), axes=FALSE)
 axis(1, labels=name, at=1:4)
 axis(2)
 
 lab <- expression('Population growth rate ('*lambda*')')
-boxplot(cbind(res1[54,1,], res2[54,1,], res3[54,1,], res4[54,1,]),
+boxplot(cbind(res1[54,1,incl], res2[54,1,incl], res3[54,1,incl], res4[54,1,incl]),
         ylab=lab,  outline= FALSE, names=name,
         col=c("red", "dodgerblue", "darkolivegreen", "orange"), axes=FALSE)
 axis(1, labels=name, at=1:4)
 axis(2)
 
 lab <- expression('SD ('*lambda*')')
-boxplot(cbind(res1[54,2,], res2[54,2,], res3[54,2,], res4[54,2,]),
+boxplot(cbind(res1[54,2,incl], res2[54,2,incl], res3[54,2,incl], res4[54,2,incl]),
         ylab=lab,  outline=FALSE, names=name,
         col=c("red", "dodgerblue", "darkolivegreen", "orange"), axes=FALSE)
 axis(1, labels=name, at=1:4)
